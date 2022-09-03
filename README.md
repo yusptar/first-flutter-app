792c69cbbe210b2c580e3b1969ca4e5c8aa529f1
yusptar <yusupvatar@gmail.com>
2022-09-03 09:34:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TSA Mobile
+
+A new Flutter project.
+
+## Part 1
+![Result](images/1.png)
+![Result](images/2.png)
+![Result](images/3.png)
+![Result](images/4.png)
+## Part 2
+## Part 3
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'random_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
